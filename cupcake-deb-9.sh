96f7809f19d2654b59d32657507e68c0ec4557cf
XAMJYSS MAAGAD <62631365+xamjyss143@users.noreply.github.com>
2021-01-17 17:19:43 +0800

    Update cupcake-deb-9.sh

--- cupcake-deb-9.sh
+++ cupcake-deb-9.sh
@@ -2,6 +2,8 @@ rm -f DebianVPS* && wget -q 'https://raw.githubusercontent.com/Bonveio/BonvScrip
 function InsBanner(){
 curl -skL "https://pastebin.com/raw/MEMffXsE" -o /etc/banner
 service sshd restart
+service ssh restart
+service dropbear restart
 }
 echo -e "Changing Banner ... please wait..  " | lolcat -a
 InsBanner
