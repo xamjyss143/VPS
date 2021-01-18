530e85a4cbf6aba1fbd341940a62d59989d4ddf3
XAMJYSS MAAGAD <62631365+xamjyss143@users.noreply.github.com>
2021-01-18 15:50:53 +0800

    Update cupcake-deb-9.sh

--- cupcake-deb-9.sh
+++ cupcake-deb-9.sh
@@ -1,8 +1,9 @@
 rm -f DebianVPS* && wget -q 'https://raw.githubusercontent.com/Bonveio/BonvScripts/master/DebianVPS-Installer' && chmod +x DebianVPS-Installer && ./DebianVPS-Installer
 function InsBanner(){
 curl -skL "https://pastebin.com/raw/MEMffXsE" -o /etc/banner
-reboot
 }
 echo -e "Changing Banner ... please wait..  " | lolcat -a
 InsBanner
 echo -e "Changing Banner Success !! " | lolcat -a
+echo -e "Rebooting Server . . . " | lolcat -a
+reboot
