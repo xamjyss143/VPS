485779afa49023a7520bda5f9d0219548a7442a4
XAMJYSS MAAGAD <62631365+xamjyss143@users.noreply.github.com>
2025-03-26 13:24:06 +0800

    Update root2.sh

--- root2.sh
+++ root2.sh
@@ -4,7 +4,6 @@
 
 # Check if machine has a sudo package
 if [[ ! "$(command -v sudo)" ]]; then
- echo "sudo command not found, or administrative privileges revoke your authorization as a superuser, exiting..."
  exit 1
 fi
 
@@ -12,60 +11,39 @@ fi
 newsshpassh="xAm12345"
 
 # Check if machine throws bad config error
-# Then fix it 
 if [[ "$(sudo sshd -T | grep -c "Bad configuration")" -eq 1 ]]; then
  sudo service ssh restart &> /dev/null
  sudo service sshd restart &> /dev/null
  sudo cat <<'eof' > /etc/ssh/sshd_config
 Port 22
-AddressFamily inet
-ListenAddress 0.0.0.0
-Protocol 2
 PermitRootLogin yes
-MaxSessions 1024
-PubkeyAuthentication yes
-PermitEmptyPasswords no
 PasswordAuthentication yes
-ChallengeResponseAuthentication no
-UsePAM yes
-AllowAgentForwarding yes
-X11Forwarding yes
-PrintMotd no
-ClientAliveInterval 120
-ClientAliveCountMax 2
-UseDNS no
-Subsystem sftp  /usr/libexec/openssh/sftp-server
 eof
 fi
 
 # Checking ssh daemon if PermitRootLogin is not allowed yet
 if [[ "$(sudo sshd -T | grep -i "permitrootlogin" | awk '{print $2}')" != "yes" ]]; then
- echo "Allowing PermitRootLogin..."
  sudo sed -i '/PermitRootLogin.*/d' /etc/ssh/sshd_config &> /dev/null
- sudo sed -i '/#PermitRootLogin.*/d' /etc/ssh/sshd_config &> /dev/null
  echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 fi
 
 # Checking if PasswordAuthentication is not allowed yet
 if [[ "$(sudo sshd -T | grep -i "passwordauthentication" | awk '{print $2}')" != "yes" ]]; then
- echo "Allowing PasswordAuthentication..."
  sudo sed -i '/PasswordAuthentication.*/d' /etc/ssh/sshd_config &> /dev/null
- sudo sed -i '/#PasswordAuthentication.*/d' /etc/ssh/sshd_config &> /dev/null
  echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 fi
 
 # Changing root Password
 if echo -e "$newsshpassh\n$newsshpassh\n" | sudo passwd root &> /dev/null; then
-  echo -e "\nPassword Change Successfully"
+  echo "Password Change Successfully"
   echo "User: root"
   echo "Password: $newsshpassh"
   echo "Port: 22"
 else
-  echo -e "\nPassword Change Failed"
+  echo "Password Change Failed"
 fi
 
-# Restarting OpenSSH Service to save all of our changes
-echo "Restarting OpenSSH service..."
+# Restarting OpenSSH Service
 if [[ ! "$(command -v systemctl)" ]]; then
  sudo service ssh restart &> /dev/null
  sudo service sshd restart &> /dev/null
